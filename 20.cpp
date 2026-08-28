ce620e7ca0e27963a65207ade9aaf47a02e77ced
AkanshaPadhi <akanshapadhi31@gmail.com>
2026-08-28 10:40:37 +0530

    20. valid parenthesis (easy/own logic) runtime 0ms
    learnt about stack implementataion;
    return st.empty() instead of if st.empty() return true;

---
+++ 20.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char>st;
+     for(int i=0;i<s.length();i++){
+     if (s[i]=='(' || s[i]=='[' || s[i]=='{'){
+     st.push(s[i]);
+     continue;
+     }
+        if(s[i]==')' || s[i]==']' || s[i]=='}')
+        if (st.empty())
+        return false;
+        if((st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']') || (st.top()=='{' && s[i]=='}') ) {
+        st.pop();
+        continue;
+        }
+        return false;
+        }
+     return st.empty();
+    }
+};
