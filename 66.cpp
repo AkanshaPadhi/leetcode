a426c868963dc5ae7eb4de3d2d26a90cd3f17b7b
AkanshaPadhi <akanshapadhi31@gmail.com>
2026-09-06 15:44:29 +0530

    66.plus one (easy/ half own logic)
    learnt about using push_back( ) to extend the size instead of digits[digits.size( )]=0

---
+++ 66.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {   
+    if(digits[digits.size()-1]!=9){
+        digits[digits.size()-1]++;
+    }else { int i=digits.size()-1;
+        while(i>=0 && digits[i]==9 ){
+           digits[i]=0;
+           i--; 
+        } if(i==-1){
+            digits[0]=1;
+            digits.push_back(0);
+        }
+        else{
+            digits[i]++;
+        }
+        }
+        return digits;
+    }
+};
